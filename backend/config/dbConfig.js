a660b69dcc0f0c6e97a6dcdc372dc8e085a77cd4
Sakthi0704 <sakthiganesh.j@yjktechnologies.com>
2026-06-27 12:01:57 +0530

    2026-06-27 eve Changes

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -6,22 +6,22 @@ module.exports = {
   server: "localhost",
   database: "sample",
   port: 1433,*/
-//   user: "saraswathi",
-//   password: "@%dSCt15",
-//   server: "95.216.47.253",
-//   database: "YJK_Admin",
-//   port: 1433,
-//   options: {
-//     encrypt: false,
-//     requestTimeout: 300000, // 5 minutes
-//     connectionTimeout: 300000, // 5 minutes
-//   },   
-  user: "YJK_DB_USER",
-  password: "YJK$ql2022",
-  server: "10.128.4.138",
+  user: "saraswathi",
+  password: "@%dSCt15",
+  server: "95.216.47.253",
   database: "YJK_Admin",
   port: 1433,
   options: {
     encrypt: false,
+    requestTimeout: 300000, // 5 minutes
+    connectionTimeout: 300000, // 5 minutes
   },   
+  // user: "YJK_DB_USER",
+  // password: "YJK$ql2022",
+  // server: "10.128.4.138",
+  // database: "YJK_Admin",
+  // port: 1433,
+  // options: {
+  //   encrypt: false,
+  // },   
 };
